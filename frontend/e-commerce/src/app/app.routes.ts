2fb461044dd9ca60785382909ab1cfde66156a5d
Anthony Ladon <ladonanthony@gmail.com>
2024-03-16 16:25:38 +0100

    add customs colors to tailwind.config.js + styling header

--- frontend/e-commerce/src/app/app.routes.ts
+++ frontend/e-commerce/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 export const routes: Routes = [
   {
-    path: "/",
-    component: AppComponent,
+    path: "",
+    component: HomeComponent,
   },
 ];
