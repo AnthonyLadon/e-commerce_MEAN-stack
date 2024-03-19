d26f36acce91a9605ef2a662a786b09575cc9c68
Anthony Ladon <ladonanthony@gmail.com>
2024-03-19 12:11:46 +0100

    add fontawesome lib localy

--- frontend/e-commerce/src/app/pages/home/home.component.ts
+++ frontend/e-commerce/src/app/pages/home/home.component.ts
@@ -4,12 +4,11 @@ import { Product } from "../../types/product";
 import { OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { CommonModule } from "@angular/common";
-import { NgIf } from "@angular/common";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [NgIf, CommonModule],
+  imports: [CommonModule],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.css",
 })
