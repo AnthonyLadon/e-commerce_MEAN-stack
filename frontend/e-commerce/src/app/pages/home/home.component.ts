e41263c0918e194e8726034510aa4b278feb6fbd
Anthony Ladon <ladonanthony@gmail.com>
2024-03-20 14:29:43 +0100

    add banner-component'

--- frontend/e-commerce/src/app/pages/home/home.component.ts
+++ frontend/e-commerce/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { ProductListComponent } from "../../components/product-list/product-list.component";
+import { BannerComponent } from "../../components/banner/banner.component";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [ProductListComponent],
+  imports: [ProductListComponent, BannerComponent],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.css",
 })
