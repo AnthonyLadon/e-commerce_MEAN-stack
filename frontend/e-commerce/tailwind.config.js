7f14c7052c711539c39d7d9a2e52cf1763741bc2
Anthony Ladon <ladonanthony@gmail.com>
2024-03-24 14:21:27 +0100

    changed navbar, add theme controller, cart, profile logo & dropdown

--- frontend/e-commerce/tailwind.config.js
+++ frontend/e-commerce/tailwind.config.js
@@ -46,7 +46,7 @@ module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
     themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "lemonade", // name of one of the included themes for dark mode
+    darkTheme: "garden", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
