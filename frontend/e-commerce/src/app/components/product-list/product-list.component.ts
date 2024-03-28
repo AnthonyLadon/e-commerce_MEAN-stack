13ef8c75279a6c77b91777fb9ab373b4860e1572
Anthony Ladon <ladonanthony@gmail.com>
2024-03-28 15:18:53 +0100

    add pagination (ngx-pagination) -> productList

--- frontend/e-commerce/src/app/components/product-list/product-list.component.ts
+++ frontend/e-commerce/src/app/components/product-list/product-list.component.ts
@@ -5,17 +5,20 @@ import { Product } from "../../types/product";
 import { OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { CommonModule } from "@angular/common";
+import { NgxPaginationModule } from "ngx-pagination";
 
 @Component({
   selector: "app-product-list",
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgxPaginationModule],
   templateUrl: "./product-list.component.html",
   styleUrl: "./product-list.component.css",
 })
 export class ProductListComponent implements OnInit {
   product$: Observable<Product[]> | null = null;
   @Input() TotalProductsFetched: number;
+  itemsPerPage: number = 10;
+  currentPage: number = 1; // default page
 
   constructor(private productService: ProductService) {}
 
