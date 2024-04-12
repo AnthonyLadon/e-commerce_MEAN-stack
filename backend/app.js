53e86dbea74b5b2d845eafdfc089d97619c5ca33
Anthony Ladon <ladonanthony@gmail.com>
2024-04-12 23:18:48 +0200

    Add dependencies for express, express-session, and mongoose in package.json

---
+++ backend/app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const session = require("express-session");
+const path = require("path"); // module qui fournit des utilitaires pour travailler avec les chemins de fichiers et de répertoires
+const app = express(); // appelle la fonction express pour créer une application Express
+const port = 3000;
+const User = require("./models/User");
+
+// ********************************************************/
+
+const mongoose = require("mongoose"); // module qui permet de se connecter à une base de données MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/easyshopping", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+/* Middleware qui initialise une session */
+app.use(
+  session(
+    { secret: "kzhVhfUJQTglG5fUFPzW8I1jaa8NNHop" }, // Clé secrète pour signer le cookie de session
+    (resave = false), // Ne sauvegarde pas la session à chaque requête
+    (saveUninitialized = false) // Ne sauvegarde pas les sessions vides
+  )
+);
+
+app.use(express.json());
+/* Parse le corps de la requête en objet JavaScript utilisable
+(bodyParser) */
+
+// ********************************************************/
+
+async function createUser() {
+  const user = new User({
+    email: "jlkj@hjh.com",
+    password: "123456",
+    age: 25,
+    firstname: "John",
+    lastname: "Doe",
+  });
+
+  // Enregistre l'utilisateur dans la base de données
+  await user1.save().then(() => console.log("Utilisateur créé !"));
+}
+
+app.get("/", (req, res) => {
+  req.session.views = req.session.views ? req.session.views + 1 : 1;
+  res.send(`<h1>Main. vu ${req.session.views} fois </h1>`);
+});
+
+app.get("/articles/:id", (req, res) => {
+  console.log("parametres: " + req.params.id);
+  console.log("queries: " + req.query);
+  res.send(`Article #${req.params.id} retourné`);
+});
+
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "images"))
+); /* Middleware qui pointe vers le dossier images et joint: __dirname + nom du dossier "images" */
+
+// 404 si aucune page trouvée
+// Si aucune route n'a renvoyé de réponse, celle-ci en renvoie une
+app.use((req, res) => {
+  res.status(404).send("Erreur 404 - page non trouvée");
+});
+
+//******* Listening to port 3000 *************************/
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
---
+++ backend/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose; // destructuring pour extraire le module schema de mongoose
+
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  age: { type: Number, min: 0 },
+});
+
+module.exports = mongoose.model("User", userSchema); // enregistre le modèle User dans la base de données
+// et exporte le modèle User pour pouvoir l'utiliser dans d'autres fichiers
